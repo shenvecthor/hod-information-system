0ef0253b0413e977982b762525c2e440cccfc8af
shenvecthor <140363918+shenvecthor@users.noreply.github.com>
2024-07-28 15:44:30 -0400

    progress update module added
    progress update module added

--- models/ProgressUpdate.js
+++ models/ProgressUpdate.js
@@ -3,13 +3,12 @@ const mongoose = require('mongoose');
 const ProgressUpdateSchema = new mongoose.Schema({
   course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
   lecturer: { type: mongoose.Schema.Types.ObjectId, ref: 'Lecturer', required: true },
-  date: { type: Date, default: Date.now },
-  content: { type: String, required: true },
-  status: {
-    type: String,
-    enum: ['On Track', 'Behind Schedule', 'Ahead of Schedule'],
-    required: true
-  }
+  gradingStatus: { type: String, required: true },
+  gradingCompletionDate: Date,
+  marksheetStatus: { type: String, required: true },
+  marksheetCompletionDate: Date,
+  marksheetSubmitted: { type: Boolean, default: false },
+  marksheetSubmissionDate: Date
 });
 
 module.exports = mongoose.model('ProgressUpdate', ProgressUpdateSchema);
